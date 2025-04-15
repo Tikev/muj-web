ea212070aaafcfb0fb2df9334047092007c28d62
vitadvo <Vitezslav9@gmail.com>
2025-04-15 22:28:21 +0200

    velka změna

--- script.js
+++ script.js
@@ -1,12 +1,3 @@
-// Funkce pro obsluhu kliknutí na tlačítko
-function handleButtonClick() {
-    alert('Tlačítko bylo kliknuto!');
-}
-
-// Přidání posluchače události na tlačítko
-document.addEventListener('DOMContentLoaded', () => {
-    const button = document.querySelector('button'); // Najde první tlačítko na stránce
-    if (button) {
-        button.addEventListener('click', handleButtonClick);
-    }
+document.getElementById("tlacitko").addEventListener("click", function() {
+    alert("Výborně, Copilote!");
 });
